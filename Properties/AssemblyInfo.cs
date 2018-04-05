d47388490e45086d10b3dd293f6f14c3d9d1bc93
Lukas Berger <mail@lukasberger.at>
2018-04-05 16:52:01 +0200

    AssemblyInfo: update native version to 0.3.0 and add new attributes

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -51,3 +51,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.3.0.0")]
 [assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyInformationalVersion("0.3.0.0")]
