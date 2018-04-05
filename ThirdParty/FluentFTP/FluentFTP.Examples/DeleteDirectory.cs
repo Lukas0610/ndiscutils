c98a4183c637bc34a2f3b5a4b7ff8b296002285d
Lukas Berger <mail@lukasberger.at>
2018-04-05 18:54:22 +0200

    imported latest FluentFTP
    Imported latest FluentFTP from https://github.com/robinrodricks/FluentFTP/commit/7a2f256b1a69cc7020462b02304b2862cfd67ec8

---
+++ ThirdParty/FluentFTP/FluentFTP.Examples/DeleteDirectory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using FluentFTP;
+
+namespace Examples {
+    static class DeleteDirectoryExample {
+        public static void DeleteDirectory() {
+            using (FtpClient conn = new FtpClient()) {
+                conn.Host = "localhost";
+                conn.Credentials = new NetworkCredential("ftptest", "ftptest");
+                
+                // Remove the directory and all objects beneath it.
+                conn.DeleteDirectory("/path/to/directory");
+            }
+        }
+    }
+}
