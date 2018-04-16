7ef95b0f85d0f010691b71b25591c4904c6703e6
Lukas0610 <mail@lukasberger.at>
2018-04-16 18:36:52 +0200

    SoftJbod: clean up

--- IO/SoftRaid/SoftJbodStream.cs
+++ IO/SoftRaid/SoftJbodStream.cs
@@ -16,10 +16,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace nDiscUtils.IO.SoftRaid
